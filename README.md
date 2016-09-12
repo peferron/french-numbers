3cb7abde7995b54957130bcd6f9d4f80ed3331d3
Pierre-Emile Ferron <peferron@gmail.com>
2016-09-12 17:04:55 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # french-numbers
+
+A simple way to train your oral comprehension of French numbers, including common years, times, and prices.
+
+Requires browser support for ECMAScript 2015 and the [Speech Synthesis API](http://caniuse.com/#feat=speech-synthesis).
---
+++ main.js
@@ -0,0 +1,91 @@
+const container = document.body;
+
+function $(selector) {
+    return container.querySelector(selector);
+}
+
+function random(min, max) {
+    return min + Math.floor(Math.random() * (max - min + 1));
+}
+
+function pad(number, minLength) {
+    const str = number.toString();
+    const missing = minLength - str.length;
+    return missing > 0 ? ' '.repeat(missing) + str : str;
+}
+
+function generateYear() {
+    return random(1980, 2020);
+}
+
+function generateTime() {
+    const hour = random(0, 23);
+    const minute = pad(random(0, 59), 2);
+    return hour + 'h' + minute;
+}
+
+function generatePrice() {
+    const integer = random(1, 999);
+
+    if (random(0,1)) {
+        return integer + '€';
+    }
+
+    const fractional = pad(random(1, 99), 2);
+    return integer + ',' + fractional + '€';
+}
+
+function generateNumber() {
+    const r = random(0, 99);
+
+    if (r < 25) {
+        return generateYear();
+    }
+
+    if (r < 50) {
+        return generateTime();
+    }
+
+    if (r < 75) {
+        return generatePrice();
+    }
+
+    return random(0, 100000);
+}
+
+function speak() {
+    const utterance = new SpeechSynthesisUtterance();
+    utterance.text = container.dataset.number;
+    utterance.lang = 'fr-FR';
+    window.speechSynthesis.speak(utterance);
+
+    $('.answer').focus();
+}
+
+function next() {
+    container.dataset.number = generateNumber();
+
+    $('.answer').value = '';
+    $('.result').classList.remove('result--ok', 'result--nok');
+    $('.solution').classList.remove('solution--show');
+    $('.solution__number').textContent = container.dataset.number;
+
+    speak();
+}
+
+function checkAnswer() {
+    const ok = $('.answer').value === container.dataset.number;
+    $('.result').classList.toggle('result--ok', ok);
+    $('.result').classList.toggle('result--nok', !ok);
+}
+
+function showSolution() {
+    $('.solution').classList.add('solution--show');
+}
+
+$('.speak').addEventListener('click', speak);
+$('.result__check').addEventListener('click', checkAnswer);
+$('.solution__show').addEventListener('click', showSolution);
+$('.next').addEventListener('click', next);
+
+next();
